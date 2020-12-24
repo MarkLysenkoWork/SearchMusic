68eb11c69fa3465204aae78cd6a7f4968b2d895a
Mark Lysenko <marklysenkowork@gmail.com>
2020-12-24 17:58:07 +0200

    Add search bar and table view. Search while putting fake results in the table

---
+++ SearchMusic/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  SearchMusic
+//
+//  Created by Mark Lysenko on 24.12.2020.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
