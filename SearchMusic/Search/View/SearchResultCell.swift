1f681723569d9069363731a2edc30acabb596303
Mark Lysenko <marklysenkowork@gmail.com>
2020-12-25 15:05:03 +0200

    style the search bar and add folders

--- SearchMusic/Search/View/SearchResultCell.swift
+++ SearchMusic/Search/View/SearchResultCell.swift
@@ -12,15 +12,18 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var artistNameLabel: UILabel!
     @IBOutlet var artworkImageView: UIImageView!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+        selectedBackgroundView = selectedView
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
     }
-
+    
 }
