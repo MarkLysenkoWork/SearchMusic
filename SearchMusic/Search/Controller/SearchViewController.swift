52a248e24ec78f6be1fee3c68d2be09d2cd66d9b
Mark Lysenko <marklysenkowork@gmail.com>
2020-12-28 19:55:07 +0200

    delete performRequest and add 1st step URLSession

--- SearchMusic/Search/Controller/SearchViewController.swift
+++ SearchMusic/Search/Controller/SearchViewController.swift
@@ -51,15 +51,6 @@ class SearchViewController: UIViewController {
         return url!
     }
     
-    func performStoreRequest(with url: URL) -> Data? {
-        do {
-            return try Data(contentsOf: url)
-        } catch {
-            print("Download Error: \(error.localizedDescription)")
-            showNetworkError()
-            return nil
-        }
-    }
     
     func parse(data: Data) -> [SearchResult] {
         do {
@@ -91,21 +82,35 @@ extension SearchViewController: UISearchBarDelegate {
             hasSearched = true
             searchResults = []
             
-
-            let url = self.iTunesURL(searchText: searchBar.text!)
-            DispatchQueue.global().async {
-                if let data = self.performStoreRequest(with: url) {
-                    self.searchResults = self.parse(data: data)
+            
+            
+            let url = iTunesURL(searchText: searchBar.text!)
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url) { data, response, error in
+                print("On main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
+                if let error = error {
+                    print("Failure! \(error.localizedDescription)")
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    if let data = data {
+                        self.searchResults = self.parse(data: data)
+                        self.searchResults.sort(by: <)
+                        DispatchQueue.main.async {
+                            self.isLoading = false
+                            self.tableView.reloadData()
+                        }
+                        return
+                    }
+                } else {
+                    print("Failure! \(response!)")
                 }
-                
-                self.searchResults.sort(by: <)
                 DispatchQueue.main.async {
+                    self.hasSearched = false
                     self.isLoading = false
                     self.tableView.reloadData()
+                    self.showNetworkError()
                 }
             }
-            
-            
+            dataTask.resume()
         }
     }
     
